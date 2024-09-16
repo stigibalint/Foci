ff56d91fcd48685db107fe724d249f6bb564b508
stiegelmayer.balint <stiegelmayer.balint@BURG-1E03-05>
2024-09-16 13:01:27 +0200

    Add project files.

---
+++ Foci/Models/FociDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Foci.Models
+{
+    public class FociDbContext : DbContext
+    { 
+    public FociDbContext(DbContextOptions<FociDbContext> options) : base(options)
+{
+    }
+    
+    public DbSet<Meccs> Meccsek { get; set; }
+    }
+}
---
+++ Foci/Models/Meccs.cs
@@ -0,0 +1,14 @@
+﻿namespace Foci.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFelido { get; set; }
+        public int VendegFelido { get; set; }
+        public string? HazaiCsapat { get; set; }
+        public string? VendegCsapat{ get; set; }
+    }
+}
